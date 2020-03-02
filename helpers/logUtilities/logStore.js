5374a820cb72663ea023de1cdbb6eb4bfe7e51ab
Sebastian Banaszkiewicz <sbanaszkiewicz@tpnyc.local>
2020-03-02 11:04:44 +0100

    minor changes

--- helpers/logUtilities/logStore.js
+++ helpers/logUtilities/logStore.js
@@ -1,7 +1,7 @@
-const bytes = require('bytes')
-const moment = require('moment')
+const bytes = require("bytes");
+const moment = require("moment");
 const Log = require("./logParser");
-const MiddlewareManager = require('./middleware');
+const MiddlewareManager = require("./middleware");
 
 class LogStore {
   constructor(init) {
@@ -30,8 +30,7 @@ class LogStore {
   calcMedian(prop) {
     const props = this.get(prop);
     props.sort((a, b) => a - b);
-    if (props.length % 2 !== 0)
-      return props[Math.floor(props.length / 2)];
+    if (props.length % 2 !== 0) return props[Math.floor(props.length / 2)];
     const index = props.length / 2;
     return (props[index] + props[index - 1]) / 2;
   }
@@ -45,7 +44,7 @@ class LogStore {
   }
 
   get(prop, val = null) {
-    if (typeof prop === 'function')
+    if (typeof prop === "function")
       return new LogStore(this.logs.filter((log, i) => prop(log.data, i)));
     if (val === null)
       return this.logs
@@ -61,39 +60,46 @@ class LogStore {
       total: this.length
     };
 
-    const forStats = ['zipSize', 'duration', 'uploadTime', 'scormProcessingTime', ]
-    const valuesToGet = ['average', 'median', 'biggest', 'lowest'];
+    const forStats = [
+      "zipSize",
+      "duration",
+      "uploadTime",
+      "scormProcessingTime"
+    ];
+    const valuesToGet = ["average", "median", "biggest", "lowest"];
 
     const reg = /^(.{1})(.+)$/;
     const firstUpper = (match, p1, p2) => `${p1.toUpperCase()}${p2}`;
     for (const stat of forStats)
       for (const value of valuesToGet) {
-        const fnName = `calc${value.replace(reg, firstUpper)}`
+        const fnName = `calc${value.replace(reg, firstUpper)}`;
         manager.use(stat, (obj, next) => {
           obj[value] = this[fnName](stat);
+          // obj.total = this.get(stat);
+          obj.total = this.get(stat).reduce((a, b) => a + b, 0);
           next();
-        })
+        });
       }
 
     // stats middlewares
     const toBytes = (obj, next) => {
-      for (const value of valuesToGet)
-        obj[value] = bytes(obj[value]);
+      for (const value of valuesToGet) obj[value] = bytes(obj[value]);
+      obj.total = bytes(obj.total);
       next();
-    }
+    };
     const humanizeDuration = (obj, next) => {
       for (const value of valuesToGet)
-        obj[value] = moment.duration(obj[value]).seconds() + ' s';
+        obj[value] = moment.duration(obj[value]).asSeconds() + " s";
+      obj.total = moment.duration(obj.total).asMinutes().toFixed(2) + " min";
       next();
-    }
+    };
 
-    manager.use('zipSize', toBytes);
-    manager.use('duration', humanizeDuration);
-    manager.use('uploadTime', humanizeDuration);
-    manager.use('scormProcessingTime', humanizeDuration);
+    manager.use("zipSize", toBytes);
+    manager.use("duration", humanizeDuration);
+    manager.use("uploadTime", humanizeDuration);
+    manager.use("scormProcessingTime", humanizeDuration);
 
-    for (const stat of forStats)
-      stats[stat] = manager.start(stat);
+    for (const stat of forStats) stats[stat] = manager.start(stat);
 
     return stats;
   }
@@ -103,8 +109,7 @@ class LogStore {
   }
 
   *[Symbol.iterator]() {
-    for (const log of this.logs)
-      yield log;
+    for (const log of this.logs) yield log;
   }
 }
 
--- helpers/stats.js
+++ helpers/stats.js
@@ -4,27 +4,20 @@ const cpuLen = require("os").cpus().length;
 process.env.UV_THREADPOOL_SIZE = cpuLen;
 
 const async = require("async");
-const {
-  pipeline
-} = require("stream");
+const { pipeline } = require("stream");
 const path = require("path");
-const {
-  promises: fs,
-  createReadStream
-} = require("fs");
+const { promises: fs, createReadStream } = require("fs");
 const split = require("split2");
 const Log = require("./logUtilities/logParser");
 const LogStore = require("./logUtilities/logStore");
-const ProgressBar = require('progress');
-const chalk = require('chalk')
+const ProgressBar = require("progress");
+const chalk = require("chalk");
 
 // get all log directories from the config file
 const config = require("../config.json");
 const dirs = [
   ...new Set(
-    config.SITES
-    .map(el => el.ENV.SCORM_LOG_PATH)
-    .concat(config.OLD_LOGS || [])
+    config.SITES.map(el => el.ENV.SCORM_LOG_PATH).concat(config.OLD_LOGS || [])
   )
 ];
 
@@ -36,23 +29,35 @@ async function main() {
   // process all log files
   await populateStore(dirs);
   // group all logs
-  // const error = store.get(log => log.error)
-  // const unfinished = store.get(log => !log.finished && !log.error)
-  const success = store.get(log => !log.error && log.finished)
-  console.log(success.getStats())
+  const error = store.get(log => log.error);
+  const unfinished = store.get(log => !log.finished && !log.error);
+  const success = store.get(log => !log.error && log.finished);
+  console.log(success.getStats());
   const users = {};
-  success.getUsers().map(user => users[user] = success.get('user', user));
+  success.getUsers().map(user => (users[user] = success.get("user", user)));
   for (const [user, userStore] of Object.entries(users)) {
-    console.log(user)
-    console.log(userStore.getStats())
+    console.log(user);
+    console.log(userStore.getStats());
   }
+  console.log(
+    `Total: ${store.length}. Success: ${success.length} (${(
+      (success.length / store.length) *
+      100
+    ).toFixed(2)}%). Unfinished: ${unfinished.length} (${(
+      (unfinished.length / store.length) *
+      100
+    ).toFixed(2)}%). Error: ${error.length} (${(
+      (error.length / store.length) *
+      100
+    ).toFixed(2)}%)`
+  );
 }
 
 async function populateStore(dirs) {
   // concatenate all log file paths to one array
   let paths = (await Promise.all(dirs.map(d => getLogPaths(d))))
     .filter(d => d)
-    .flat()
+    .flat();
   // read all logs and populate the log store
   await readLogs(paths);
 }
@@ -70,12 +75,15 @@ async function getLogPaths(dir) {
 
 async function readLogs(logs) {
   try {
-    const bar = new ProgressBar('Parsing logs [:bar] :current/:total (:percent) [:elapsed s]', {
-      total: logs.length,
-      width: 25,
-      complete: chalk.green('='),
-      incomplete: chalk.grey('-')
-    });
+    const bar = new ProgressBar(
+      "Parsing logs [:bar] :current/:total (:percent) [:elapsed s]",
+      {
+        total: logs.length,
+        width: 25,
+        complete: chalk.green("="),
+        incomplete: chalk.grey("-")
+      }
+    );
     // parse all logs
     await async.eachLimit(
       logs,
@@ -84,19 +92,19 @@ async function readLogs(logs) {
         new Promise((resolve, reject) => {
           // Log class extends Writable stream, so we can pipe it
           const parser = new Log(log);
-          pipeline(
-            createReadStream(log),
-            split(),
-            parser,
-            err => {
-              bar.tick();
-              if (err) return reject(err);
-              store.add(parser);
-              resolve();
-            });
+          pipeline(createReadStream(log), split(), parser, err => {
+            bar.tick();
+            if (err) return reject(err);
+            store.add(parser);
+            resolve();
+          });
         })
     );
-    console.log(`Parsed ${chalk.yellow(store.logs.reduce((a, log) => a + log.lines.length, 0))} lines of logs!`)
+    console.log(
+      `Parsed ${chalk.yellow(
+        store.logs.reduce((a, log) => a + log.lines.length, 0)
+      )} lines of logs!`
+    );
   } catch (err) {
     console.error(`Error while processing: ${err.message}`);
   }
